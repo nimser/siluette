b2fc1b543009c5f484bb41722de80bb233e7b178
Nicolas Wormser <nicolas@codewithflair.org>
2023-09-17 13:56:24 +0200

    Add test descriptions

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,38 @@
 import { render, screen } from "@testing-library/react"
 import App from "./App"
-describe("test the App root component", () => {
-  it("should render the UI", () => {
-    render(<App />)
+
+beforeEach(() => {
+  render(<App />)
+})
+describe("happy path", () => {
+  it.skip("should fill-up form and show customized avatar ", () => {
+    // type name
+    // pick team
+    // upload avatar
+    // pick three favorite techs
+    // click Done
+    // form should be hidden
+  })
+})
+
+describe("App component", () => {
+  it.skip("should render the UI", () => {
     expect(screen.getByText(/Customize your avatar/)).toBeInTheDocument()
+  })
+
+  it.skip("should preview name when typing", () => {
+    //
+  })
+  it.skip("should set team color in preview when selecting team", () => {
+    //
+  })
+  it.skip("should preview image when uploading", () => {
+    //
+  })
+  it.skip("should set icons for interrests when selected", () => {
+    //
+  })
+  it.skip("should prevent clicking Done if Some info isn't set", () => {
+    //
   })
 })
---
+++ src/CheckboxGroup.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react"
+import CheckboxGroup from "./CheckBoxGroup"
+
+describe("CheckboxGroup component", () => {
+  it.skip("should set max amount of choices based on props", () => {
+    //
+  })
+})
